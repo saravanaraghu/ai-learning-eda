c14fd70846baab7eedff762ad271a39f15ecb3c6
Saravana Akshay <saravanaraghu7@gmail.com>
2026-02-24 15:46:01 +0530

    Added a csv and used pandas to get data

--- ai-learning-eda/eda.py
+++ ai-learning-eda/eda.py
@@ -1,4 +1,7 @@
-a = int(input())
-
-
-print(a)
+import pandas as pd
+import os
+os.chdir(
+    r"C:\Users\RaghuswamySaravana.A\projects\ai-learning-eda"
+    )
+df = pd.read_csv("employee_data_200.csv")
+print(df.describe())
