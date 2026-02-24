111e7ed6b3f7fce70daf8f72120ebb1723abd366
RaghuswamySaravana Akshay <raghuswamysaravana.akshay@wpp.com>
2026-02-24 17:16:04 +0530

    Added few lines to the operations that can be performed with pandas

---
+++ ai-learning-eda/analysis/eda.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Locating the dataframe
+df = pd.read_csv("data/employee_data_200.csv")
+print("Data loaded successfully")
+print(df.head())
+
+
+#Shapes and summary
+print("\nShape:", df.shape)
+print("\nColumn info:")
+print(df.info())
+print("\nStatistical Summary: ", df.describe())
+
+# Data checks
+
+print("\n Missing Values: ", df.isna().sum())
+print("\n Duplicate rows:", df.duplicated().sum())
+
+# Feauture
+
+print("\n Department Distribution: ", df["Department"].value_counts())
+print("\n Maximum Salary: \n", df.loc[(df.groupby("Department")["Salary"].idxmax()), ["FullName", "Department", "Salary"]])
+print("\n Minimum Salary: \n", df.loc[(df.groupby("Department")["Salary"].idxmin()), ["FullName", "Department", "Salary"]])
--- ai-learning-eda/eda.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-import os
-df = pd.read_csv("employee_data_200.csv")
-print(df.describe())
