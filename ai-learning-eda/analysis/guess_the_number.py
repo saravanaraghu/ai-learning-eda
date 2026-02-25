e0ff7f560123f7dca044e913e798b969fd1c996f
RaghuswamySaravana Akshay <raghuswamysaravana.akshay@wpp.com>
2026-02-25 22:32:34 +0530

    created a new file that is used to guess the random number

---
+++ ai-learning-eda/analysis/guess_the_number.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+x = np.random.randint(100)
+
+z = True
+
+print("Guess number between 0 to 99")
+
+while(z):
+    y = int(input("Enter a number to guess"))
+    
+    if(y==x):
+        print("Your guess is correct")
+        z=False
+    elif(y>x):
+        print("Your guess is huge bro")
+    else:
+        print("Your guess is short bro")
