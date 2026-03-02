1a05843e8c40543eb041be0eb62d3bcc80bfc084
RaghuswamySaravana Akshay <raghuswamysaravana.akshay@wpp.com>
2026-03-02 11:33:14 +0530

    Rearranged lines in guess_the_number.py file

--- ai-learning-eda/analysis/guess_the_number.py
+++ ai-learning-eda/analysis/guess_the_number.py
@@ -6,13 +6,13 @@ z = True
 
 print("Guess number between 0 to 10")
 
-while(z):
+while z:
     y = int(input("Enter a number to guess"))
-    
-    if(y==x):
+
+    if y == x:
         print("Your guess is correct")
-        z=False
-    elif(y>x):
+        z = False
+    elif y > x:
         print("Your guess is huge bro")
     else:
         print("Your guess is short bro")
